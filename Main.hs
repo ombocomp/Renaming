92485d24cd59a6352231383516054a0e3786ea61
jtapolczai <janos.tapolczai@gmail.com>
2014-04-17 19:25:59 +0200

    Added sortByDate-pipe to Main.

--- Main.hs
+++ Main.hs
@@ -8,16 +8,14 @@ import Data.Maybe
 import qualified Data.List as LS
 import Control.Monad.FunctionGraph
 import Data.Renaming
+import Data.Ord
 import System.IO
-import System.FilePath (combine, splitExtensions)
+import System.FilePath (combine)
 import Text.Read
 import qualified Algorithms.NaturalSort as NS (compare)
 
-
-
 main :: IO ()
 main =
-   let print' n (old, new) = putStrLn $ padRight n old ++ " --> " ++ new  in
    do dir <- askForBy "Enter directory name: " "Directory not found!" doesDirectoryExist
       files' <- getDirectoryContents dir
       let files = LS.sortBy NS.compare files'
@@ -25,24 +23,23 @@ main =
                                                                   isJust .
                                                                   flip lookup pipeLib)
       let pipe = fromJust $ lookup pipeName pipeLib
-      renamings <- pipe files
+      renamings <- pipe dir files
       putStrLn "Proposed renamings: "
       mapM_ (print' (maximum $ map length files)) renamings
       proceed <- askFor "Proceed (True/False)? " "True/False required!"
-      let addDir = combine dir *** combine dir
+      let addDir = combine dir *** liftM (combine dir)
       if proceed then renameFiles (map addDir renamings) >> putStrLn "Finished."
       else putStrLn "Exiting without action."
 
-
+print' :: Int -> (String, Either String String) -> IO ()
+print' n (old, new) = putStrLn $ padRight n old ++ "-->" ++ show new
 
 askFor :: Read a => String -> String -> IO a
 askFor prompt err = askForBy' prompt err (const (return True))
 
 askForBy' :: Read a => String -> String -> (a -> IO Bool) -> IO a
 askForBy' prompt err check = do
-   putStr prompt
-   hFlush stdout
-   x <- getLine
+   x <- putPrompt prompt
    case readMaybe x of
       Just x' -> do isValid <- check x'
                     if isValid then return x'
@@ -51,22 +48,31 @@ askForBy' prompt err check = do
 
 askForBy :: String -> String -> (String -> IO Bool) -> IO String
 askForBy prompt err check = do
-   putStr prompt
-   hFlush stdout
-   x <- getLine
+   x <- putPrompt prompt
    isValid <- check x
    if isValid then return x
    else putStrLn err >> askForBy prompt err check
 
+putPrompt :: String -> IO String
+putPrompt prompt = putStr prompt >> hFlush stdout >> getLine
+
 padRight :: Int -> String -> String
 padRight n xs = xs ++ replicate (n - length xs) ' '
 
-pipeLib :: [(String, [String] -> IO [(String, String)])]
+pipeLib :: [(String, String -> [String] -> IO [(String, Either String String)])]
 pipeLib = [
-   ("addN", \files ->
+   ("addN", \_ files ->
       do amount <- askFor "Enter amount: " "Integer required!"
          let f = mapPar $ mapInt (+amount)
-         return $ applyRenamings f files),
-   ("renameCD", \files ->
-      return $ applyRenamings (mapPar $ mapName (take 2)) files)
+         applyRenamings f files),
+   ("renameCD", const $ applyRenamings (mapPar $ mapName (take 2))),
+   ("sortByDate", \dir files ->
+      do let modtime x = getModificationTime (combine dir x)
+         let sorter = mapPar (addInfoM modtime >=> flattenET)
+                      =|> sortBy (comparing snd)
+                      =|> mapPar (liftP fst)
+                      =|> mapPar (splitExt >< liftP snd)
+                      =|> addInfoG (map show ([1..]::[Integer]))
+                      =|> mapPar (liftP $ \(e,n) -> (n++e))
+         applyRenamings sorter files)
    ]
