82542b5cab0d15be11efc1d344ce7c203ac7f878
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-05-16 22:53:21 +0200

    Rewrote the console UI to use StateT.

--- Main.hs
+++ Main.hs
@@ -6,17 +6,90 @@ import Control.Monad
 import Control.Arrow
 import Control.Arrow.ParArrow
 import Control.Arrow.Utils
+import Control.Monad.State.Lazy
 import Data.Maybe
 import qualified Data.List as LS
 import Data.Renaming
 import Data.Ord
 import System.IO
 import System.FilePath (combine)
-import Text.Read
+import Text.Read hiding (get)
 import qualified Algorithms.NaturalSort as NS (compare)
 
 data Asc = Asc | Desc deriving (Show, Eq, Ord, Read, Enum)
 
+type AppState = StateT String IO ()
+
+main :: IO ()
+main = getCurrentDirectory >>= evalStateT main'
+  where
+    main' :: AppState
+    main' = repl' isExitCommand (liftIO $ putPrompt "> ") runCommand
+
+    runCommand c = fromMaybe (liftIO $ putStrLn "Command not found. Type :help for a list of commands.")
+                             (lookup c commandLib)
+
+    isExitCommand = (`elem` exitCommands) . trim
+      where exitCommands = [":e", ":exit", ":quit", ":q"]
+
+    trim = unwords . words
+
+
+commandLib :: [(String, AppState)]
+commandLib = [
+   (":help", do liftIO $ putStrLn "Renamer"
+                liftIO $ putStrLn "Commands: :cd, :pipe, :exit"
+                dir <- get
+                liftIO $ putStrLn $ "Current directory: " ++ dir
+   ),
+   -- Change current directory
+   (":cd", do dir <- liftIO $ askForBy "Enter new directory: "
+                                       "Directory not found!"
+                                       doesDirectoryExist
+              put dir
+   ),
+   -- Run a pipe
+   (":pipe", do dir <- get
+                liftIO (
+                  do files' <- getDirectoryContents dir
+                     let files = LS.sortBy NS.compare files'
+                     pipeName <- askForBy "Enter pipe name: "
+                                          "Pipe not found!"
+                                          (return
+                                           . isJust
+                                           . flip lookup pipeLib)
+                     let pipe = fromJust $ lookup pipeName pipeLib
+                     renamings <- pipe dir files
+                     putStrLn "Proposed renamings: "
+                     mapM_ (print' (maximum $ map length files)) renamings
+                     proceed <- askFor "Proceed (True/False)? " "True/False required!"
+                     let addDir = combine dir *** liftM (combine dir)
+                     if proceed then do renameFiles (map addDir renamings)
+                                        putStrLn "Finished."
+                                else putStrLn "Didn't do anything.")
+      )
+   ]
+
+-- |Executes a REP loop until a given condition is met.
+--  Variant of @repl'@ which performs the same input and output action
+--  over and over again.
+repl' :: Monad m => (a -> Bool) -> m a -> (a -> m b) -> m ()
+repl' check f g = repl check (repeat f) (repeat g)
+
+-- |Executes a REP loop until a given condition is met.
+repl :: Monad m
+     => (a -> Bool) -- |The check which, when true, terminates the loop.
+     -> [m a] -- |The list of inputs.
+     -> [a -> m b] -- |The list of functions operating on the input.
+     -> m ()
+repl check i o = sequenceWhile check $ zip i o
+  where sequenceWhile _ [] = return ()
+        sequenceWhile f ((inp,out):xs) =
+           do inp' <- inp
+              unless (f inp') $ do out inp'
+                                   sequenceWhile f xs
+
+{-
 main :: IO ()
 main =
    do dir <- askForBy "Enter directory name: " "Directory not found!" doesDirectoryExist
@@ -33,6 +106,8 @@ main =
       let addDir = combine dir *** liftM (combine dir)
       if proceed then renameFiles (map addDir renamings) >> putStrLn "Finished."
       else putStrLn "Exiting without action."
+
+-}
 
 print' :: Int -> (String, Either String String) -> IO ()
 print' n (old, new) = putStrLn $ padRight n old ++ "-->" ++ show new
@@ -88,6 +163,7 @@ pipeLib = [
                  =|> mapPar (switch <> (liftP show, liftP id) >< addExt)
          applyRenamings f files)
    ]
+
 
 
 -- For testing
